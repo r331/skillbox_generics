4a8bd808378ae19fe248b488e0919edf2f4e6e3f
Roman Ivanov <roivanoff@gmail.com>
2021-09-23 16:19:28 +0300

    kljkl

--- src/ru/skillbox/part8/Main.java
+++ src/ru/skillbox/part8/Main.java
@@ -8,7 +8,7 @@ public class Main {
     static List<String> heapPollution() {
         List numbers = new ArrayList<Number>();
         numbers.add(1);
-        List<String> ls = numbers; // (1)
+        List<String> ls = numbers;
         ls.add("stroka");
         return ls;
     }
@@ -16,7 +16,8 @@ public class Main {
     static void heapPollution2(List<String>... stringLists) {
         Object[] array = stringLists;
         List<Integer> tmpList = Arrays.asList(42);
-        array[0] = tmpList; // (1)
-        String s = stringLists[0].get(0); // (2)
+        array[0] = tmpList;
+        String str = stringLists[0].get(0);
+        System.out.println(str);
     }
 }
